56e673a815deb0888a0fb63bb4ab65d25881b992
Simon Adams <simonadams@gmail.com>
2023-01-16 22:37:36 +0000

    Update run.ts
    Updated to include ability to have a hard 'earliest' date to support tarrif change overs.

--- src/run.ts
+++ src/run.ts
@@ -9,9 +9,22 @@ export async function run(
     octopusApi: OctopusAPI,
     tariffs: Tariff[]
 ): Promise<void> {
+    const hardFromDateString = process.env.HARD_FROM_DATE;
+    const hardFromDateInit = hardFromDateString ? new Date(hardFromDateString) : null;
+    const hardFromDate = hardFromDateInit && !isNaN(hardFromDateInit.getTime()) ? hardFromDateInit : null;
+
     while (true) {
         console.log(`Poll started at ${new Date().toISOString()}`);
-        const from: Date = moment().subtract(3, "month").toDate();
+        if (hardFromDate) {
+            console.log(`With hard from date ${hardFromDate.toISOString()}`);
+        }
+        const fromInit: Date = moment().subtract(3, "month").toDate();
+        let from = fromInit;
+        if (hardFromDate && from < hardFromDate) {
+            from = hardFromDate;
+        }
+        console.log(`Polling from ${from.toISOString()}`);
+
         await Poller.poll(from, tariffs, octopusApi, influxApi);
         console.log(`Poll finished at ${new Date().toISOString()}`);
         await sleep(30 * 60 * 1000);
